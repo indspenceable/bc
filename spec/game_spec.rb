606336740504e6d791af272cf756e6b7075c3be8
Daniel Spencer <dspencer@squareup.com>
2013-01-17 20:29:20 -0800

    Some starter specs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,19 +1,37 @@
 require 'spec_helper'
-
-class Game
-end
+require_relative File.join("..", "lib", "game")
 
 describe Game do
+  subject { Game.new }
   context "A new game" do
-    it "asks both players to select their characters"
-    it "doesn't reveal character choices until both players have selected"
+    it "asks both players to select their characters" do
+      subject.required_input.should == {
+        0 => "select_character",
+        1 => "select_character",
+      }
+    end
+    it "doesn't reveal character choices until both players have selected" do
+      subject.input!(0, "hikaru")
+      subject.required_input.should == {
+        1 => "select_character"
+      }
+      subject.game_state.events.should be_empty
+    end
   end
   context "playing a game with characters selected" do
     before :each do
-      # character selection code here.
+      # these should probably be test characters?
+      subject.input!(0, "hikaru")
+      subject.input!(1, "hikaru")
     end
     context "setup" do
-      it "asks both players to choose discards"
+      it "asks both players to choose discards" do
+        subject.required_input.should == {
+        0 => "select_discards",
+        1 => "select_discards",
+      }
+      end
+      it "blah blah blah more tests?"
     end
     context "from the start of beat 0" do
       before :each do
