be13ef60b2e1197c83ea666f69fd0ba86b92cb7b
Daniel Spencer <dspencer@squareup.com>
2013-02-01 15:57:58 -0800

    Players are now stored in an array, rather than in seperate variables.

--- lib/game.rb
+++ lib/game.rb
@@ -179,6 +179,7 @@ class Game
   # player_id - that players game_state
   # if you hand it a player_id, it will provide you more information
   def game_state(player_id=nil)
+    return {:events => []} unless @players
     {
       :events => @events,
       :players => [
@@ -207,40 +208,57 @@ class Game
       ->(text) { Game.character_names.include?(text) },
       ->(text) { Game.character_names.include?(text) }
     )
+    @players = [nil, nil]
     # for now, just consume input.
-    @player0 = Game.character_list[
+    @players[0] = Game.character_list[
       Game.character_names.index @input_manager.answer(0)].new(
         0,
         @input_manager,
         @events)
-    @player1 = Game.character_list[
+    @players[1] = Game.character_list[
       Game.character_names.index @input_manager.answer(1)].new(
         1,
         @input_manager,
         @events)
 
-    @player0.opponent = @player1
-    @player1.opponent = @player0
+    @players[0].opponent = @players[1]
+    @players[1].opponent = @players[0]
   end
   def select_discards!
-    # select_discards
-    @input_manager.require_multi_input!("select_discards",
-      # these should shell out to characters individual methods, for roberts
-      # sake.
-      @player0.valid_discard_callback,
-      @player1.valid_discard_callback
+    # # select_discards
+    # @input_manager.require_multi_input!("select_discards",
+    #   # these should shell out to characters individual methods, for roberts
+    #   # sake.
+    #   @players[0].valid_discard_callback,
+    #   @players[1].valid_discard_callback
+    # )
+    # @players[0].set_initial_discards!(@input_manager.answer(0))
+    # @players[1].set_initial_discards!(@input_manager.answer(1))
+
+    @input_manager.require_multi_input!("select_attack_pairs",
+      @players[0].valid_attack_pair_callback,
+      @players[1].valid_attack_pair_callback,
     )
-    @player0.set_initial_discards!(@input_manager.answer(0))
-    @player1.set_initial_discards!(@input_manager.answer(1))
+    p0a0 = @input_manager.answer(0)
+    p1a0 = @input_manager.answer(1)
+    @input_manager.require_multi_input!("select_attack_pairs",
+      @players[0].valid_attack_pair_callback,
+      @players[1].valid_attack_pair_callback,
+    )
+    p0a1 = @input_manager.answer(0)
+    p1a1 = @input_manager.answer(1)
+
+    @players[0].set_initial_discards!("#{p0a0}{p0a1}")
+    @players[1].set_initial_discards!("#{p1a0}{p1a1}")
   end
 
   def select_attack_pairs!
     @input_manager.require_multi_input!("select_attack_pairs",
-      @player0.valid_attack_pair_callback,
-      @player1.valid_attack_pair_callback
+      @players[0].valid_attack_pair_callback,
+      @players[1].valid_attack_pair_callback
     )
-    @player0.set_attack_pair!(@input_manager.answer(0))
-    @player1.set_attack_pair!(@input_manager.answer(1))
+    @players[0].set_attack_pair!(@input_manager.answer(0))
+    @players[1].set_attack_pair!(@input_manager.answer(1))
   end
 
   def ante!
@@ -249,7 +267,7 @@ class Game
     while number_of_passes < 2
       passed_this_round = true
       # ugly...
-      current_player = current_player_id == 0 ? @player0 : @player1
+      current_player = current_player_id == 0 ? @players[0] : @players[1]
 
       # if they can ante, make them. If they don't pass, note that,
       # and enact the ante
@@ -274,30 +292,30 @@ class Game
   end
 
   def reveal!
-    log_event!("Reveal", "Player 1 plays #{@player0.reveal_attack_pair!}", "Player 2 plays #{@player1.reveal_attack_pair!}")
+    log_event!("Reveal", "Player 1 plays #{@players[0].reveal_attack_pair!}", "Player 2 plays #{@players[1].reveal_attack_pair!}")
   end
 
   def handle_clashes!
-    while @player0.priority == @player1.priority
+    while @players[0].priority == @players[1].priority
       log_event!("Clash!!")
-      return :no_cards if (@player0.no_cards? || @player1.no_cards?)
+      return :no_cards if (@players[0].no_cards? || @players[1].no_cards?)
       puts "----------------------------------------------clashtest----------------------------"
       @input_manager.require_multi_input!("select_new_base",
-        @player0.base_options_callback,
-        @player1.base_options_callback
+        @players[0].base_options_callback,
+        @players[1].base_options_callback
       )
-      @player0.select_new_base!(@input_manager.answer(0))
-      @player1.select_new_base!(@input_manager.answer(1))
+      @players[0].select_new_base!(@input_manager.answer(0))
+      @players[1].select_new_base!(@input_manager.answer(1))
       reveal!
     end
   end
 
   def determine_active_player!
     # at this point, we know someone won priority
-    if @player0.priority > @player1.priority
-      @active_player, @reactive_player = @player0, @player1
+    if @players[0].priority > @players[1].priority
+      @active_player, @reactive_player = @players[0], @players[1]
     else
-      @active_player, @reactive_player = @player1, @player0
+      @active_player, @reactive_player = @players[1], @players[0]
     end
     #some characters care if they are active...
     log_event!("Player #{@active_player.character_id} is the active player")
@@ -352,7 +370,7 @@ class Game
   # this adds more information if player_id and as_seen_by_id match
   def player_info_for(player_id, as_seen_by_id)
     {
-      :location => @player_locations[player_id]
+      :location => @players[player_id].position
     }
   end
 end
