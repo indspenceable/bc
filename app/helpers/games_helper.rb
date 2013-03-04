61c4ae3dd82de098b0bea01cb5312ec105c23f41
Daniel Spencer <dspencer@squareup.com>
2013-03-04 00:08:30 -0800

    factors out badge logic, and adds badges for beta characters.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,4 +4,15 @@ module GamesHelper
     badges["Input Required"] = :info if game.play.required_input[game.player_id(current_user)]
     badges
   end
+  def character_badges(character_name)
+    badges = {}
+    badges["Beta"] = :warning if ['khadath', 'rukyuk'].include?(character_name)
+    badges
+  end
+
+  def badges(badges)
+    badges.map do |badge, color|
+      content_tag(:span, class: "label label-#{color}"){ badge }
+    end.join("").html_safe
+  end
 end
