9e6d57d1d9a33657fb2e9f5b596724f52e29c929
Daniel Spencer <dspencer@squareup.com>
2013-03-03 21:04:44 -0800

    Dynamically insert domain into persona login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def login
     return if session[:email]
-    json_from_persona = `curl -d "assertion=#{params[:assertion]}&audience=localhost" "https://verifier.login.persona.org/verify"`
+    json_from_persona = `curl -d "assertion=#{params[:assertion]}&audience=#{request.protocol}#{request.host_with_port}" "https://verifier.login.persona.org/verify"`
     json_from_persona = JSON.parse(json_from_persona)
     if json_from_persona['status'] == "okay"
       flash[:notice] = "Logged in!"
