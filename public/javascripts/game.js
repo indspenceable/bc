e799a43ef4b96a8ef7c16146e5623da7d8427074
Daniel Spencer <dspencer@squareup.com>
2013-03-09 10:30:49 -0800

    Fixes some passive effects to display better in the UI.

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -45,44 +45,44 @@ var init = function(player_id, game_id, character_names) {
     geomantic: makeCard(0, 1, 0, {"Start of Beat": "You may ante another token for this beat."}),
     focused: makeCard(0, 1, 0, {"On Hit": "Recover an elemental token of your choice."}),
     trance: makeCard("0~1", 0, 0, {"Start of Beat": "Return all anted tokens to your pool. You don't get their effects this turn.", "End of Beat": "Recover an elemental token of your choice."}),
-    sweeping: makeCard(0, -1, 3, {"passive": "If hikaru gets hit this turn, he takes 2 additional damage."}),
+    sweeping: makeCard(0, -1, 3, {"If hikaru gets hit this turn, he takes 2 additional damage.": undefined}),
     advancing: makeCard(0, 1, 1, {"Start of beat": "Advance 1 space. If this causes you to switch sides with an opponent, you get +1 power this beat."}),
     // Finishers
     wrathofelements: makeCard(1, 7, 6, {"Reveal": "Immediately ante all available tokens and apply them to this attack."}),
     fourwinds: makeCard(1, 2, 5, {"Before Activating": "Advance up to one space.", "On Hit": "Regain an elemental token. If you do, repeat this attack."}),
 
     //Cadenza
-    battery: makeCard(0, 1, -1, {"passive": "You get +4 priority next beat."}),
+    battery: makeCard(0, 1, -1, {"You get +4 priority next beat.": undefined}),
     clockwork: makeCard(0, 3, -3, {"Soak": 3}),
     hydraulic: makeCard(0, 2, -1, {"Before Activating": "Advance 1 space.", "Soak": 1}),
     mechanical: makeCard(0, 2, -2, {"End of Beat": "Advance up to 3 spaces."}),
     grapnel: makeCard("2~4", 0, 0, {"On Hit": "Pull opponent up to 3 spaces."}),
-    press: makeCard("1~2", 1, 0, {"Stun Guard": 2, "passive": "Deals additional damage equal to the ammount of damage you have taken this beat."}),
+    press: makeCard("1~2", 1, 0, {"Stun Guard": 2, "+1 damage for each point of damage you have taken this beat.": undefined}),
     // Finishers
-    rocketpress: makeCard(1, 8, 0, {"Soak": 3, "passive": "Stun Immunity", "Before Activating": "Advance 2 or more spaces."}),
+    rocketpress: makeCard(1, 8, 0, {"Soak": 3, "Stun Immunity": undefined, "Before Activating": "Advance 2 or more spaces."}),
     feedbackfield: makeCard("1~2", 1, 0, {"Soak": 5, "On Hit": "This attack has +2 power for each point of damage Cadenza soaked this beat."}),
 
     // Khadath
     hunters: makeCard(0, 1, 0, {"reveal": "+2 Power +2 Priority if opponent is on or next to your trap."}),
-    teleport: makeCard("0~2", 1, -4, {"passive": "Ranged attacks don't hit you if your trap is between you and your opponnent.", "End of Beat": "Move to any unoccupied space. Move your trap to any unoccupied space."}),
+    teleport: makeCard("0~2", 1, -4, {"Ranged attacks don't hit you if your trap is between you and your opponnent.": undefined, "End of Beat": "Move to any unoccupied space. Move your trap to any unoccupied space."}),
     blight: makeCard("0~2", 0, 0, {"Start of Beat": "Place your trap anywhere in your range."}),
     evacuation: makeCard("0~1", 0, 0, {"Start of Beat": "Place your trap in your current location, then retreat 1 space."}),
     lure: makeCard("0~5", -1, -1, {"On Hit": "Pull your opponent any number of spaces."}),
-    snare: makeCard("X", 3, 1, {"passive": "This attack hits opponents on or adjacent to your trap. You can't move your trap this beat.", "Stun Immunity": undefined}),
+    snare: makeCard("X", 3, 1, {"The range of this attack includes the space occupied by your trap, and the adjacent spaces.": undefined, "You can't move your trap this beat.": undefined, "Stun Immunity": undefined}),
     // Finishers
     dimensionalexile: makeCard("N/A", 25, 0, {"Stun Immunity. The range of this attack is the space occupied by Khadath's trap.": undefined}),
     planarcrossing: makeCard("1~2", 4, 5, {"On Hit": "Rearrange the board however you like, moving all characters and markers to legal positions."}),
 
     // Rukyuk
-    trick: makeCard("1~2", 0, -3, {'passive': "Stun Immunity"}),
+    trick: makeCard("1~2", 0, -3, {"Stun Immunity": undefined}),
     sniper: makeCard("3~6", 1, 2, {"After Activating": "Move 1, 2, or 3 spaces"}),
     crossfire: makeCard("2~3", 0, -2, {"Soak": 2, "On Hit": "Discard any token from your ammo pool for +2 power this beat."}),
     pointblank: makeCard("0~1", 0, 0, {"Stun Guard": 2, "On Damage": "Push your opponent up to 2 spaces"}),
     gunner: makeCard("2~4", 0, 0, {"Before Activating": "Discard any ammo token from your ammo pool for -1~+1 range this beat.", "After Activating": "Move 1 or 2 spaces."}),
     reload: makeCard("N/A", "N/A", 4, {"After Actvating": "Move directly to any unoccupied space.", "End of Beat": "Recover all ammo tokens."}),
     // Finishers
-    fullyautomatic: makeCard("3~6", 2, 6, {"passive": "Do not apply the effects of ammo tokens to this attack.", "On Hit":"You may discard an ammo token to execute this attack again."}),
-    forcegrenade: makeCard("1~2", 4, 4, {"pasive": "Do not apply the effects of ammo tokens to this attack. This attack hits even if an ammo token was not anted.", "On Hit": "Push the opponent any number of spaces.", "After Activating": "Retreat any number of spaces."}),
+    fullyautomatic: makeCard("3~6", 2, 6, {"Do not apply the effects of ammo tokens to this attack.": undefined, "On Hit":"You may discard an ammo token to execute this attack again."}),
+    forcegrenade: makeCard("1~2", 4, 4, {"Do not apply the effects of ammo tokens to this attack. This attack hits even if an ammo token was not anted.": undefined, "On Hit": "Push the opponent any number of spaces.", "After Activating": "Retreat any number of spaces."}),
   }
   var loadCard = function(cardName, $card, overrideCardName) {
     $card.find('.name').text(overrideCardName || capitaliseFirstLetter(cardName))
