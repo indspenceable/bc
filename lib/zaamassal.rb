ab8c880e5bba56e42299562435f416ed80495455
Daniel Spencer <dspencer@squareup.com>
2013-03-13 13:14:34 -0700

    Fluidity is move, not advance.

--- lib/zaamassal.rb
+++ lib/zaamassal.rb
@@ -115,12 +115,12 @@ class Fluidity < Paradigm
   end
   def before_activating!
     {
-      "movement" => select_from_methods(advance: [0,1])
+      "movement" => select_from_methods(advance: [0,1], retreat: [1])
     }
   end
   def end_of_beat!
     {
-      "movement" => select_from_methods(advance: [0,1])
+      "movement" => select_from_methods(advance: [0,1], retreat: [1])
     }
   end
   def name_and_effect
