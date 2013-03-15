550dc5fda0a2d922ecbbcd76986cf6da6974caff
Daniel Spencer <dspencer@squareup.com>
2013-03-15 01:24:54 -0700

    adds data migration script.

---
+++ script/migrate_to_select_from_movement_methods.rb
@@ -0,0 +1,20 @@
+movement_methods = MOVEMENT_METHODS.map(&:to_s)
+Game.transaction do
+  Game.where(active: false).each do |g|
+    inputs = g.inputs
+    g.inputs = []
+    play = g.play
+    inputs.each do |pl, val|
+      if val.include?('#')
+        meth, arg = val.split('#')
+        if movement_methods.include?(meth)
+          play.input!(pl, play.characters[pl].send("#{meth}?", Integer(arg)).to_s)
+          next
+        end
+      end
+      play.input!(pl, val)
+    end
+    g.inputs = play.valid_inputs
+    g.save!
+  end
+end
